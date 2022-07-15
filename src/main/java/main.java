a09104d9a63deef6b827d28db24e57abd3a34e12
JorgeMontoya <Jamontoyao@uqvirtual.edu.co>
2022-07-15 16:41:59 -0500

    se corrige nombre de la clase para conexion a bd

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,15 @@
+import org.jboss.logging.Logger;
 public class main {
     public static void main(String[] args) {
      MySQL conection = new MySQL();
+
+        try {
+            conection.getConnection();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        conection.getpregunta("1");
 
     }
 }
