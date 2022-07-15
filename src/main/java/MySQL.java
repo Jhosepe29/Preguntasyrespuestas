800a4e8be357a60dc8ac17e7109af84231d531db
JorgeMontoya <Jamontoyao@uqvirtual.edu.co>
2022-07-15 16:16:11 -0500

    se establece la conexion a la base de datos mySQL

--- src/main/java/MySQL.java
+++ src/main/java/MySQL.java
@@ -1,2 +1,70 @@
+import com.mysql.cj.xdevapi.Statement;
+import org.jboss.logging.Logger;
+
+import javax.swing.*;
+import java.io.PrintStream;
+import java.sql.*;
+
 public class MySQL {
+
+     Logger escribirEnConsola = Logger.getLogger(MySQL.class) ;
+
+
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "admin";
+
+    private static final String URL = "jdbc:mysql://localhost/preguntas_respuestas?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrival=true";
+
+
+    static{
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null,"Error en el driver" + e);
+        }
+    }
+
+
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            connection= DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Connection failed" + e);
+        }
+        return connection;
+    }
+
+
+
+    private static final Connection CONNECTION = getConnection();
+    final PrintStream PRINT_STREAM = new PrintStream(System.out);
+
+
+
+    public String getpregunta (String idRandom) {
+
+        String query = "SELECT * FROM preguntas_respuestas.preguntas WHERE id = ?";
+        ResultSet rs;
+        String salidaConsulta= "";
+        try (PreparedStatement ps = CONNECTION.prepareStatement(query)) {
+            ps.setInt(1, Integer.parseInt(idRandom));
+            rs = ps.executeQuery();
+            salidaConsulta = rs.getString("preguntas");
+
+
+        } catch (SQLException e) {
+
+            escribirEnConsola.info(e);
+        }
+        return salidaConsulta;
+    }
+
+
+
+
 }
